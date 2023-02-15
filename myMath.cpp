ed3aaa0f31b1c1ea32d5c3003730b47a710bc933
Muhammed Magdy Abdelhady <57032945+Muhammed-Magdy-Abdelhady@users.noreply.github.com>
2023-02-15 22:14:23 +0200

    adding min

--- myMath.cpp
+++ myMath.cpp
@@ -8,3 +8,14 @@ int getSum(std::vector<int> numbers)
     }
     return sum;
 }
+
+int getMin(std::vector<int> numbers)
+{
+    int min = numbers[0];
+    for (const auto& number : numbers){
+        if (number < min){
+            min = number;
+        }
+    }
+    return min;
+}
--- myMath.h
+++ myMath.h
@@ -2,5 +2,5 @@
 #include <vector>
 
 int getSum(std::vector<int> numbers);
-
+int getMin(std::vector<int> numbers);
 
